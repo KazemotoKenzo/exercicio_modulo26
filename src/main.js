6fa159278bb07711d5ad1e8ca7dbd20c3ee0bb7c
Vitor Hashimoto <kenzo80804@gmail.com>
2024-12-27 04:24:55 -0300

    exercicio_modulo_26

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(numA, numB) {
+    return numA * numB;
+}
+function saudacao(nome) {
+    return 'Ola ' + nome;
+}
+console.log(multiplicacao(10, 2));
+console.log(saudacao('Kenzo'));
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(numA: number, numB:number):number{
+    return numA*numB;
+}
+
+function saudacao(nome:string):string{
+    return 'Ola ' + nome;
+}
+
+console.log(multiplicacao(10,2));
+console.log(saudacao('Kenzo'));
